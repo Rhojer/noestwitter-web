0e4985cf7b2bcc66e5ca490bcdfa93bb1fb4c9fc
DEScontrol <34425397+Rhojer@users.noreply.github.com>
2024-08-28 12:06:24 -0400

    integrando y probando el component ui - PrimeReact

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'primereact/button';
+import {Card} from 'primereact/card'
+import { Divider } from 'primereact/divider';
+import { Menubar } from 'primereact/menubar';
+
+
 import './App.css';
 
+const items = [
+  {
+      label: 'Home',
+      icon: 'pi pi-home'
+  },
+  {
+      label: 'Features',
+      icon: 'pi pi-star'
+  },
+  {
+      label: 'Projects',
+      icon: 'pi pi-search',
+      items: [
+          {
+              label: 'Core',
+              icon: 'pi pi-bolt',
+              shortcut: '⌘+S'
+          },
+          {
+              label: 'Blocks',
+              icon: 'pi pi-server',
+              shortcut: '⌘+B'
+          },
+          {
+              label: 'UI Kit',
+              icon: 'pi pi-pencil',
+              shortcut: '⌘+U'
+          },
+          {
+              separator: true
+          },
+          {
+              label: 'Templates',
+              icon: 'pi pi-palette',
+              items: [
+                  {
+                      label: 'Apollo',
+                      icon: 'pi pi-palette',
+                      badge: 2
+                  },
+                  {
+                      label: 'Ultima',
+                      icon: 'pi pi-palette',
+                      badge: 3
+                  }
+              ]
+          }
+      ]
+  },
+  {
+      label: 'Contact',
+      icon: 'pi pi-envelope',
+      badge: 3
+  }
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+      <Menubar model={items} />
+      </div>
+      <Card title="probando primereact">
+    <p className="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+    </p>
+</Card>
+<Divider />
+<Button label="Primary" />
+<Divider />
+<Button label="Secondary" severity="secondary" />
+<Divider />
+<Button label="Success" severity="success" />
+<Divider />
+<Button label="Info" severity="info" />
+<Divider />
+<Button label="Warning" severity="warning" />
+<Divider />
+<Button label="Help" severity="help" />
+<Divider />
+<Button label="Danger" severity="danger" />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PrimeReactProvider, PrimeReactContext} from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <PrimeReactProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </PrimeReactProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
